925863e8ca765405842d12af6b1aee43764a29e8
DomGreen <greendom99@gmail.com>
2026-07-26 22:23:54 -0700

    cleaned up unnecessary casts, explicitly typed bit shift ints as uint, fixed error checking line in gpio_init function

--- drivers/gpio/gpio_driver.c
+++ drivers/gpio/gpio_driver.c
@@ -13,40 +13,40 @@
 #include "gpio_driver.h" 
 
 void set_pin_mode(GPIO_t *gpio_port, uint8_t pin, GPIO_pin_mode_t mode){
-    ((GPIO_t *)gpio_port)->MODER &= ~(0x03 << (pin * 2)); // Clear the mode bits for the specified pin
-    ((GPIO_t *)gpio_port)->MODER |= (mode << (pin * 2)); // Set the new mode for the specified pin
+    gpio_port->MODER &= ~(0x03 << (pin * 2)); // Clear the mode bits for the specified pin
+    gpio_port->MODER |= (mode << (pin * 2)); // Set the new mode for the specified pin
 }
 
 void set_pin_output_type(GPIO_t *gpio_port, uint8_t pin, GPIO_pin_output_type_t output_type){
-    ((GPIO_t *)gpio_port)->OTYPER &= ~(0x01 << pin); // Clear the output type bit for the specified pin
-    ((GPIO_t *)gpio_port)->OTYPER |= (output_type << pin); // Set the new output type for the specified pin
+    gpio_port->OTYPER &= ~(0x01 << pin); // Clear the output type bit for the specified pin
+    gpio_port->OTYPER |= (output_type << pin); // Set the new output type for the specified pin
 }
 
 void set_pin_speed(GPIO_t *gpio_port, uint8_t pin, GPIO_pin_speed_t speed){
-    ((GPIO_t *)gpio_port)->OSPEEDR &= ~(0x03 << (pin * 2)); // Clear the speed bits for the specified pin
-    ((GPIO_t *)gpio_port)->OSPEEDR |= (speed << (pin * 2)); // Set the new speed for the specified pin
+    gpio_port->OSPEEDR &= ~(0x03 << (pin * 2)); // Clear the speed bits for the specified pin
+    gpio_port->OSPEEDR |= (speed << (pin * 2)); // Set the new speed for the specified pin
 }
 
 void set_pin_pull(GPIO_t *gpio_port, uint8_t pin, GPIO_pin_pull_t pull){
-    ((GPIO_t *)gpio_port)->PUPDR &= ~(0x03 << (pin * 2)); // Clear the pull-up/pull-down bits for the specified pin
-    ((GPIO_t *)gpio_port)->PUPDR |= (pull << (pin * 2)); // Set the new pull-up/pull-down configuration for the specified pin
+    gpio_port->PUPDR &= ~(0x03 << (pin * 2)); // Clear the pull-up/pull-down bits for the specified pin
+    gpio_port->PUPDR |= (pull << (pin * 2)); // Set the new pull-up/pull-down configuration for the specified pin
 }
 
 void set_pin_alternate_function(GPIO_t *gpio_port, uint8_t pin, GPIO_pin_alternate_function_t af){
     if(pin < 8){
-        ((GPIO_t *)gpio_port)->AFRL &= ~(0x0F << (pin * 4)); // Clear the alternate function bits for the specified pin
-        ((GPIO_t *)gpio_port)->AFRL |= (af << (pin * 4)); // Set the new alternate function for the specified pin
+        gpio_port->AFRL &= ~(0x0F << (pin * 4)); // Clear the alternate function bits for the specified pin
+        gpio_port->AFRL |= (af << (pin * 4)); // Set the new alternate function for the specified pin
     } else {
-        ((GPIO_t *)gpio_port)->AFRH &= ~(0x0F << ((pin - 8) * 4)); // Clear the alternate function bits for the specified pin
-        ((GPIO_t *)gpio_port)->AFRH |= (af << ((pin - 8) * 4)); // Set the new alternate function for the specified pin
+        gpio_port->AFRH &= ~(0x0F << ((pin - 8) * 4)); // Clear the alternate function bits for the specified pin
+        gpio_port->AFRH |= (af << ((pin - 8) * 4)); // Set the new alternate function for the specified pin
     }
 }
 
 void set_pin_state(GPIO_t *gpio_port, uint8_t pin, GPIO_pin_set_reset_t set_reset){
     if(set_reset == SET){
-        ((GPIO_t *)gpio_port)->BSRR = (1UL << pin); // Set the set bit for the specified pin
+        gpio_port->BSRR = (1UL << pin); // Set the set bit for the specified pin
     } else{
-        ((GPIO_t *)gpio_port)->BSRR = (1UL << (pin + 16)); // Set the reset bit for the specified pin
+        gpio_port->BSRR = (1UL << (pin + 16)); // Set the reset bit for the specified pin
     }
 }
 
@@ -67,12 +67,13 @@ void set_clock_state(clock_status_t clock_state){
 GPIO_status_t gpio_init(GPIO_t *gpio_port, const GPIO_config_t *config){
 
     //Error checking//
-    if ((gpio_port == NULL)||(config->pin_mask == 0U)){return GPIO_ERROR_INVALID_PIN;}
+    if ((gpio_port == NULL)||(config == NULL)){return GPIO_ERROR_INVALID_PIN;}
+    if (config->pin_mask == 0U){return GPIO_ERROR_INVALID_PIN;}
 
     //port init loop//
     for(uint8_t pin = 0; pin < 16; pin++){
         //Pin mask check to speed up loop//
-        if((config->pin_mask & (1 << pin)) == 0U) {continue;}
+        if((config->pin_mask & (1U << pin)) == 0U) {continue;}
         //otherwise  init pin//
         set_pin_mode(gpio_port, pin, config->mode);
         set_pin_output_type(gpio_port, pin, config->output_type);
