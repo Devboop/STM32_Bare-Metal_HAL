e47aa22a0980bc5e66fc6b5da1e64dfbadbaa6f9
DomGreen <greendom99@gmail.com>
2026-07-26 20:09:00 -0700

    .

--- drivers/gpio/gpio_driver.c
+++ drivers/gpio/gpio_driver.c
@@ -1,3 +1,13 @@
+/**
+ * @file gpio_driver.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2026-07-26
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #include <stdint.h>
 #include <stddef.h>
 #include "gpio_driver.h" 
