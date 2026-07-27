18fb9a3335ae9b9d1b98724c50fc734ee9751bb9
DomGreen <greendom99@gmail.com>
2026-07-26 19:34:22 -0700

    added doxygen comments

--- drivers/gpio/gpio_driver.h
+++ drivers/gpio/gpio_driver.h
@@ -3,10 +3,14 @@
 
 #include <stdint.h>
 
-// RCC base address for clock control //
+/**
+ * @brief RCC base address for clock control
+ */
 #define RCC (*(volatile uint32_t *)0x40023800UL)
 
-// GPIO base addresses for STM32F4 series microcontrollers //
+/**
+ * @brief GPIO port base addresses
+ */
 #define GPIOA ((GPIO_t *)0x40020000UL)
 #define GPIOB ((GPIO_t *)0x40020400UL)
 #define GPIOC ((GPIO_t *)0x40020800UL)
@@ -17,27 +21,33 @@
 #define GPIOH ((GPIO_t *)0x40021C00UL)
 
 
-// Bit manipulation macros //
+/**
+ * @brief Bit manipulation macros
+ */
 #define SET_BIT(gpio_reg, bit) ((gpio_reg) |= (1UL << (bit)))
 #define CLEAR_BIT(gpio_reg, bit) ((gpio_reg) &= ~(1UL << (bit)))
 #define READ_BIT(gpio_reg, bit) (((gpio_reg) >> (bit)) & 0x01)
 #define TOGGLE_BIT(gpio_reg, bit) ((gpio_reg) ^= (1 << (bit)))
 
-// GPIO register structure definition //
+/**
+ * @brief GPIO register structure definition
+ */
 typedef struct{
-volatile uint32_t MODER;
-volatile uint32_t OTYPER;
-volatile uint32_t OSPEEDR;
-volatile uint32_t PUPDR;
-volatile uint32_t IDR;
-volatile uint32_t ODR;
-volatile uint32_t BSRR;
-volatile uint32_t LCKR;
-volatile uint32_t AFRL;
-volatile uint32_t AFRH;
+volatile uint32_t MODER; /**< Mode register */
+volatile uint32_t OTYPER; /**< Output type register */
+volatile uint32_t OSPEEDR; /**< Output speed register */
+volatile uint32_t PUPDR; /**< Pull-up/pull-down register */
+volatile uint32_t IDR; /**< Input data register */
+volatile uint32_t ODR; /**< Output data register */
+volatile uint32_t BSRR; /**< Bit set/reset register */
+volatile uint32_t LCKR; /**< Lock register */
+volatile uint32_t AFRL; /**< Alternate function low register */
+volatile uint32_t AFRH; /**< Alternate function high register */
 } GPIO_t;
 
-// GPIO Status enumeration definition //
+/**
+ * @brief GPIO status enumeration
+ */
 typedef enum{
     GPIO_SUCCESS = 0,
     GPIO_ERROR_INVALID_PIN = 1,
@@ -45,7 +55,9 @@ typedef enum{
     GPIO_ERROR_INVALID_OUTPUT_TYPE = 3
 } GPIO_status_t;
 
-// GPIO pin mode enumeration definition //
+/**
+ * @brief GPIO pin mode enumeration
+ */
 typedef enum{
     INPUT = 0x00,
     OUTPUT = 0x01,
@@ -53,13 +65,17 @@ typedef enum{
     ANALOG = 0x03
 } GPIO_pin_mode_t;
 
-// GPIO pin output type enumeration definition //
+/**
+ * @brief GPIO pin output type enumeration
+ */
 typedef enum{
     PUSH_PULL = 0x00,
     OPEN_DRAIN = 0x01
 } GPIO_pin_output_type_t;
 
-// GPIO pin speed enumeration definition //
+/**
+ * @brief GPIO pin speed enumeration
+ */
 typedef enum{
     LOW = 0x00,
     MEDIUM = 0x01,
@@ -67,14 +83,18 @@ typedef enum{
     VERY_HIGH = 0x03
 } GPIO_pin_speed_t;
 
-// GPIO pin pull-up/pull-down enumeration definition //
+/**
+ * @brief GPIO pin pull-up/pull-down enumeration
+ */
 typedef enum{
     NO_PULL = 0x00,
     PULL_UP = 0x01,
     PULL_DOWN = 0x02
 } GPIO_pin_pull_t;
 
-// GPIO pin alternate function enumeration definition //
+/**
+ * @brief GPIO pin alternate function enumeration
+ */
 typedef enum{
     AF0 = 0x00,
     AF1 = 0x01,
@@ -94,50 +114,102 @@ typedef enum{
     AF15 = 0x0F
 } GPIO_pin_alternate_function_t;
 
-// GPIO pin set/reset enumeration definition //
+/**
+ * @brief GPIO pin set/reset enumeration
+ */
 typedef enum{
     RESET,
     SET
 } GPIO_pin_set_reset_t;
 
-// Clock enum//
+/**
+ * @brief Clock status enumeration
+ */
 typedef enum {
     OFF = 0x0,
     ON = 0x1
 } clock_status_t;
 
-// GPIO Config Structure definition //
+/**
+ * @brief GPIO configuration structure definition
+ */
 typedef struct{
-    uint16_t pin_mask; // Pin mask for the GPIO pins to be configured
-    GPIO_pin_mode_t mode; // Mode for the GPIO pins (input, output, alternate function, analog)
-    GPIO_pin_output_type_t output_type; // Output type for the GPIO pins (push-pull, open-drain)
-    GPIO_pin_speed_t speed; // Speed for the GPIO pins (low, medium, high, very high)
-    GPIO_pin_pull_t pull; // Pull-up/pull-down configuration for the GPIO pins (no pull, pull-up, pull-down)   
-    GPIO_pin_alternate_function_t alternate_function; // Alternate function for the GPIO pins (AF0 to AF15)
-    GPIO_pin_set_reset_t pin_state; // Set/reset configuration for the GPIO pins (set, reset)
+    uint16_t pin_mask; /**< Pin mask for the GPIO pins to be configured */
+    GPIO_pin_mode_t mode; /**< Mode for the GPIO pins (input, output, alternate function, analog) */
+    GPIO_pin_output_type_t output_type; /**< Output type for the GPIO pins (push-pull, open-drain) */
+    GPIO_pin_speed_t speed; /**< Speed for the GPIO pins (low, medium, high, very high) */
+    GPIO_pin_pull_t pull; /**< Pull-up/pull-down configuration for the GPIO pins (no pull, pull-up, pull-down) */   
+    GPIO_pin_alternate_function_t alternate_function; /**< Alternate function for the GPIO pins (AF0 to AF15) */
+    GPIO_pin_set_reset_t pin_state; /**< Set/reset configuration for the GPIO pins (set, reset) */
 } GPIO_config_t;
 
-// Set GPIO pin mode function definition //
+/**
+ * @brief GPIO pin mode function definition
+ * @param gpio_reg The GPIO register to modify
+ * @param pin The pin number (0-15) to configure
+ * @param mode The mode to set for the specified pin (input, output, alternate function, analog)
+ * @return void
+ */
 void set_pin_mode(uint32_t gpio_reg, uint8_t pin, GPIO_pin_mode_t mode);
 
-// Set GPIO pin output type function definition //
+/**
+ * @brief GPIO pin output type function definition
+ * @param gpio_reg The GPIO register to modify
+ * @param pin The pin number (0-15) to configure
+ * @param output_type The output type to set for the specified pin (push-pull, open-drain)
+ * @return void
+ */
 void set_pin_output_type(uint32_t gpio_reg, uint8_t pin, GPIO_pin_output_type_t output_type);
 
-// Set GPIO pin speed function definition //
+/**
+ * @brief GPIO pin speed function definition
+ * @param gpio_reg The GPIO register to modify
+ * @param pin The pin number (0-15) to configure
+ * @param speed The speed to set for the specified pin (low, medium, high, very high)
+ * @return void
+ */
 void set_pin_speed(uint32_t gpio_reg, uint8_t pin, GPIO_pin_speed_t speed);
 
-// Set GPIO pin pull-up/pull-down function definition //
+/**
+ * @brief GPIO pin pull-up/pull-down function definition
+ * @param gpio_reg The GPIO register to modify
+ * @param pin The pin number (0-15) to configure
+ * @param pull The pull-up/pull-down configuration to set for the specified pin (no pull, pull-up, pull-down)
+ * @return void
+ */
 void set_pin_pull(uint32_t gpio_reg, uint8_t pin, GPIO_pin_pull_t pull);
 
-// Set GPIO pin alternate function function definition //
+/**
+ * @brief GPIO pin alternate function function definition
+ * @param gpio_port The GPIO port to modify
+ * @param pin The pin number (0-15) to configure
+ * @param af The alternate function to set for the specified pin (AF0 to AF15)
+ * @return void
+ */
 void set_pin_alternate_function(GPIO_t *gpio_port, uint8_t pin, GPIO_pin_alternate_function_t af);
 
-// Bit set/reset function definition //
+/**
+ * @brief Bit set/reset function definition
+ * @param gpio_reg The GPIO register to modify
+ * @param pin The pin number (0-15) to configure
+ * @param set_reset The set/reset configuration to apply for the specified pin (set, reset)
+ * @return void
+ */
 void set_pin_state(uint32_t gpio_reg, uint8_t pin, GPIO_pin_set_reset_t set_reset);
 
-void clock_init();
+/**
+ * @brief Clock control function definition
+ * @param clock_state The clock state to set (ON, OFF)
+ * @return void
+ */
+void set_clock_state(clock_status_t clock_state);
 
-//GPIO initialization function definition //
+/**
+ * @brief GPIO initialization function definition
+ * @param gpio_port The GPIO port to initialize
+ * @param config The configuration structure containing the settings for the GPIO pins
+ * @return GPIO_status_t The status of the GPIO initialization (success or error code)
+ */
 GPIO_status_t gpio_init(GPIO_t *gpio_port, const GPIO_config_t *config);
 
 #endif // GPIO_DRIVER_H
